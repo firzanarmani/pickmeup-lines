d38393dfed1ae0974e9add60ca476d3ddae8884c
Firzan Armani <mrfirzanarmani@gmail.com>
2020-10-04 19:29:18 +0800

    Set default port if none specified

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,6 @@ import app from './config/app'
 
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
